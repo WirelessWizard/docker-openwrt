b61de9e6d0cfb16b2db2da1387e527e1d247a4b1
Nico Maas <xenomorph150@gmail.com>
2016-03-25 00:30:19 +0100

    Add Release 15.05.1 and new Trunk Version

---
+++ 15.05.1_x86/wget.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://downloads.openwrt.org/chaos_calmer/15.05.1/x86/generic/openwrt-15.05.1-x86-generic-Generic-rootfs.tar.gz -O openwrt-15.05.1-x86-generic-Generic-rootfs.tar.gz
+
