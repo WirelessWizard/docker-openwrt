e1621222db78581f28448323c65dfbe39d8fa1c4
Nico Maas <maas@uni-trier.de>
2017-01-02 21:49:02 +0100

    Add new 15.05.1 and trunk versions today (02.01.2017), compiled for x86, x64, rpi1, rpi2, rpi3

--- trunk_x64/wget.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget http://downloads.openwrt.org/snapshots/trunk/x86/64/openwrt-x86-64-generic-rootfs.tar.gz -O openwrt-x86-64-generic-rootfs.tar.gz 
--- trunk_x86/wget.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget http://downloads.openwrt.org/snapshots/trunk/x86/generic/openwrt-x86-generic-generic-rootfs.tar.gz -O openwrt-x86-generic-generic-rootfs.tar.gz
